d3daad7e4eb3495128fb34d3f8676c17b8f81869
YoungMoo Kim <rladuan612@gmail.com>
2025-08-22 19:41:25 +0900

    Camera Ray

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,8 +4,14 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    Ray lastRay;
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
