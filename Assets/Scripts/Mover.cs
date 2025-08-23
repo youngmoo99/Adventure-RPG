73aca49b93e03c186dc35df259f8ac4b50314bf8
YoungMoo Kim <rladuan612@gmail.com>
2025-08-23 19:24:12 +0900

    add Character Move Animation

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,17 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    Ray lastRay;
+
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
+        }    
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {   
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
-        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+    void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed",speed);
     }
 }
