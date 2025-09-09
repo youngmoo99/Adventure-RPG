f3ae8a3ba0f648f469b5ae3d912f20ffc02b465d
YoungMoo Kim <rladuan612@gmail.com>
2025-09-09 21:04:33 +0900

    Create Fade scene

---
+++ Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentObjectPrefab;
+
+        static bool hasSpawned = false;
+
+        void Awake()
+        {
+            if (hasSpawned) return;
+            SpawnPersistentObjects();
+
+            hasSpawned = true;
+        }
+
+        void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+
+        }
+    }
+}
