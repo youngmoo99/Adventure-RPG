7b1fc48fd2f355e673ddbc47abeb66cfbdac49b7
YoungMoo Kim <rladuan612@gmail.com>
2025-10-29 02:13:58 +0900

    Update soldier

--- Assets/Scripts/Stats/CharacterClass.cs
+++ Assets/Scripts/Stats/CharacterClass.cs
@@ -1,12 +1,13 @@
 namespace RPG.Stats
-{   
+{
     // 캐릭터 분류 : Progression 테이블에서 스탯 곡선 선택에 사용
     public enum CharacterClass
     {
         Player,
         Grunt,
         Mage,
-        Archer
+        Archer,
+        Knight
     }
 }
 
